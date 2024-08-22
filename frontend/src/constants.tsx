df1cfd27bde948cdfe14b217210677d6fd73701b
GiGi <gigiwangjob@gmail.com>
2024-08-21 19:21:51 -0500

    fix build url

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -1,3 +1,3 @@
 export const API_URL =
-  "http://taskmanagerapp-etbbeya8gag8c7c9.eastus-01.azurewebsites.net/api/tasks";
+  "https://taskmanagerapp-etbbeya8gag8c7c9.eastus-01.azurewebsites.net/api/tasks";
 export const TASK_STATUSES = ["todo", "in-progress", "done"] as const;
